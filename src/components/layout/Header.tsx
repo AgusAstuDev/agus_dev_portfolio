3c885c143fcc004870e2dc67392fb73b1f7fb5f9
AgusAstuDev <agusastudillo.dev@gmail.com>
2024-01-25 17:21:37 -0300

    Buttons liks ready with hover line and conditional asPath

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,7 @@
 // React-Next:
 import React from "react";
 import { useContext } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 // Context:
 import { langContext } from "@/context/langContext";
@@ -9,16 +10,55 @@ import { TEXTS_HOME } from "@/languages/texts_home";
 // Components:
 import Logo from "./Logo";
 
+interface CustomLinkProps {
+  href: string;
+  text: string;
+  className: string;
+}
+
+const CustomLink = ({ href, text, className }: CustomLinkProps) => {
+  const router = useRouter();
+
+  console.log("router" + router.asPath);
+
+  return (
+    <Link href={href} className={`${className} relative group `}>
+      {text}
+      <span
+        className={`absolute h-[2px] bg-dark inline-block left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      ></span>
+    </Link>
+  );
+};
+
 export default function Header() {
   const { locale } = useContext(langContext);
   const { home, about, experience, contact } = TEXTS_HOME[locale];
   return (
     <header className="w-full px-32 py-8 font-medium flex justify-between items-center">
       <nav>
-        <Link href={"/"}>{home}</Link>
-        <Link href={"/about"}>{about}</Link>
-        <Link href={"/experience"}>{experience}</Link>
-        <Link href={"/contact"}>{contact}</Link>
+        <CustomLink
+          href={"/"}
+          text={home}
+          className="w-max h-auto mr-4 inline-block"
+        />
+        <CustomLink
+          href={"/about"}
+          text={about}
+          className="w-max h-auto mr-4 inline-block"
+        />
+        <CustomLink
+          href={"/experience"}
+          text={experience}
+          className="w-max h-auto mr-4 inline-block"
+        />
+        <CustomLink
+          href={"/contact"}
+          text={contact}
+          className="w-max h-auto mr-4 inline-block"
+        />
       </nav>
       <nav>
         <Link href={"/"}>RS</Link>
