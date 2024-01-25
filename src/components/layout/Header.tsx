95fb55a08482c4b9ea7af428a12ec73e99165789
AgusAstuDev <agusastudillo.dev@gmail.com>
2024-01-25 00:02:44 -0300

    Effects and principal logo added, need fix some details

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,25 +6,29 @@ import Link from "next/link";
 import { langContext } from "@/context/langContext";
 // Texts:
 import { TEXTS_HOME } from "@/languages/texts_home";
+// Components:
+import Logo from "./Logo";
 
 export default function Header() {
   const { locale } = useContext(langContext);
   const { home, about, experience, contact } = TEXTS_HOME[locale];
   return (
-    <header className="w-full px-32 py-8 font-medium flex justify-between">
+    <header className="w-full px-32 py-8 font-medium flex justify-between items-center">
       <nav>
         <Link href={"/"}>{home}</Link>
         <Link href={"/about"}>{about}</Link>
         <Link href={"/experience"}>{experience}</Link>
         <Link href={"/contact"}>{contact}</Link>
       </nav>
-      <h2>Logo</h2>
       <nav>
         <Link href={"/"}>RS</Link>
         <Link href={"/"}>RS</Link>
         <Link href={"/"}>RS</Link>
         <Link href={"/"}>RS</Link>
       </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
     </header>
   );
 }
---
+++ src/components/layout/Logo.tsx
@@ -0,0 +1,29 @@
+// React-Next:
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+// Assets
+import logo_agus from "@/assets/layout/logoAgusAstu.png";
+// Framer motion
+import { motion } from "framer-motion";
+
+export default function Logo() {
+  return (
+    <motion.div
+      className=" w-24 h-auto mt-2"
+      whileHover={{
+        scale: 1.1,
+        rotate: 360,
+      }}
+      transition={{ duration: 2, type: "spring" }}
+    >
+      <Link href={"/"} className="w-full h-full">
+        <Image
+          src={logo_agus}
+          alt="Logo Agus"
+          className="w-full h-auto object-cover object-center"
+        ></Image>
+      </Link>
+    </motion.div>
+  );
+}
